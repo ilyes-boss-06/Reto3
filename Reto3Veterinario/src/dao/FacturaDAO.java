8ac31a7510e6772f35ee82129c56e2a258667b42
1DAM <1DAM@A11-PC30>
2026-05-13 13:59:31 +0200

    Merge branch 'main' of https://github.com/ilyes-boss-06/Reto3.git into main

---
+++ Reto3Veterinario/src/dao/FacturaDAO.java
@@ -0,0 +1,39 @@
+package dao;
+
+import java.util.List;
+
+import modelo.Factura;
+
+public class FacturaDAO implements GenericDAO<Factura> {
+
+	@Override
+	public boolean insertar(Factura objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Factura> obtenerTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Factura obtenerPorId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean actualizar(Factura objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean eliminar(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ Reto3Veterinario/src/dao/LineaFacturaDAO.java
@@ -0,0 +1,39 @@
+package dao;
+
+import java.util.List;
+
+import modelo.LineaFactura;
+
+public class LineaFacturaDAO implements GenericDAO<LineaFactura>{
+
+	@Override
+	public boolean insertar(LineaFactura objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<LineaFactura> obtenerTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LineaFactura obtenerPorId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean actualizar(LineaFactura objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean eliminar(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
