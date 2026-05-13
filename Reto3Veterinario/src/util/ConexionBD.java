6b279bf1c2543feb1da5dac82f9c30531851a4d6
1DAM <1DAM@A11-PC29>
2026-05-13 13:15:11 +0200

    hola

---
+++ Reto3Veterinario/src/util/ConexionBD.java
@@ -0,0 +1,21 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+	private static final String URL = "jdbc:mysql://localhost:3306/dam_programacion";
+	private static final String USER = "dam";
+	private static final String PASS = "1234";
+
+	public static Connection getConnection() {
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(URL, USER, PASS);
+		} catch (SQLException e) {
+			System.out.println("Error al conectar " + e.getMessage());
+		}
+		return con;
+	}
+}
