07ff13ce34d7d8130b10da7e5fb2a6d77f4f78bf
ilyes-boss-06 <Boss@DESKTOP-7E1S7DH>
2026-05-15 09:00:59 +0200

    Modelos completos

---
+++ Reto3Veterinario/src/modelo/Cliente.java
@@ -0,0 +1,47 @@
+package modelo;
+
+public class Cliente extends Persona {
+
+	private int idCliente;
+	private int idPersona;
+	private String tenefono;
+	public Cliente(int idPersona, String dni, String nombre, int idCliente, int idPersona2, String tenefono) {
+		super(idPersona, dni, nombre);
+		this.idCliente = idCliente;
+		idPersona = idPersona2;
+		this.tenefono = tenefono;
+	}
+	public Cliente(int idPersona, String dni, String nombre, int idPersona2, String tenefono) {
+		super(idPersona, dni, nombre);
+		idPersona = idPersona2;
+		this.tenefono = tenefono;
+	}
+	public Cliente(int idPersona, String dni, String nombre) {
+		super(idPersona, dni, nombre);
+	}
+	public int getIdCliente() {
+		return idCliente;
+	}
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+	public int getIdPersona() {
+		return idPersona;
+	}
+	public void setIdPersona(int idPersona) {
+		this.idPersona = idPersona;
+	}
+	public String getTenefono() {
+		return tenefono;
+	}
+	public void setTenefono(String tenefono) {
+		this.tenefono = tenefono;
+	}
+	@Override
+	public String toString() {
+		return "Cliente [idCliente=" + idCliente + ", idPersona=" + idPersona + ", tenefono=" + tenefono
+				+ ", toString()=" + super.toString() + "]";
+	}
+	
+	
+}
--- Reto3Veterinario/src/modelo/Persona.java
+++ Reto3Veterinario/src/modelo/Persona.java
@@ -2,4 +2,45 @@ package modelo;
 
 public class Persona {
 
+	private int idPersona;
+	private String dni;
+	private String nombre;
+	public Persona(int idPersona, String dni, String nombre) {
+		super();
+		this.idPersona = idPersona;
+		this.dni = dni;
+		this.nombre = nombre;
+	}
+	public Persona(String dni, String nombre) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+	}
+	public Persona() {
+		super();
+	}
+	public int getIdPersona() {
+		return idPersona;
+	}
+	public void setIdPersona(int idPersona) {
+		this.idPersona = idPersona;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	@Override
+	public String toString() {
+		return "Persona [idPersona=" + idPersona + ", dni=" + dni + ", nombre=" + nombre + "]";
+	}
+	
+	
 }
