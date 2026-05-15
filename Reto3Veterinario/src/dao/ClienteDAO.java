3486a396ec6d7771a4f2379fc6405cfb1bee944d
ilyes-boss-06 <Boss@DESKTOP-7E1S7DH>
2026-05-15 09:13:30 +0200

    ClienteDAO + VeterinarioDAO

---
+++ Reto3Veterinario/src/dao/ClienteDAO.java
@@ -0,0 +1,39 @@
+package dao;
+
+import java.util.List;
+
+import modelo.Cliente;
+
+public class ClienteDAO implements GenericDAO<Cliente>{
+
+	@Override
+	public boolean insertar(Cliente objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Cliente> obtenerTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cliente obtenerPorId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean actualizar(Cliente objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean eliminar(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ Reto3Veterinario/src/dao/VeterinarioDAO.java
@@ -0,0 +1,39 @@
+package dao;
+
+import java.util.List;
+
+import modelo.Veterinario;
+
+public class VeterinarioDAO implements GenericDAO<Veterinario>{
+
+	@Override
+	public boolean insertar(Veterinario objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Veterinario> obtenerTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Veterinario obtenerPorId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean actualizar(Veterinario objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean eliminar(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
