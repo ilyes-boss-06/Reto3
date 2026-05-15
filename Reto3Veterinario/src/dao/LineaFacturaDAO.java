6c00280556ac10289e0e54cb41edde861a96dd87
1DAM <1DAM@A11-PC30>
2026-05-15 09:22:45 +0200

    LineaFactura

--- Reto3Veterinario/src/dao/LineaFacturaDAO.java
+++ Reto3Veterinario/src/dao/LineaFacturaDAO.java
@@ -1,8 +1,15 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import modelo.LineaFactura;
+import util.ConexionBD;
 
 public class LineaFacturaDAO implements GenericDAO<LineaFactura>{
 
@@ -14,8 +21,29 @@ public class LineaFacturaDAO implements GenericDAO<LineaFactura>{
 
 	@Override
 	public List<LineaFactura> obtenerTodos() {
-		// TODO Auto-generated method stub
+		List<LineaFactura> lista = new ArrayList<LineaFactura>();
+		String sql = "Select * from Lineas_Factura";
+		   try (Connection con = ConexionBD.getConnection();
+		        PreparedStatement ps = con.prepareStatement(sql)) {
+		        ResultSet rs = ps.executeQuery();
+		    while (rs.next()) {
+		          lista.add(mapear(rs));
+		    }
+		    return lista;
+		    
+		    } catch (SQLException e) {
+		            System.out.println("Error: " + e.getMessage());
+		    }
+
+	
+		   
+
+		
+		
 		return null;
+		
+		
+
 	}
 
 	@Override
@@ -35,5 +63,20 @@ public class LineaFacturaDAO implements GenericDAO<LineaFactura>{
 		// TODO Auto-generated method stub
 		return false;
 	}
+	
+
+
+    private LineaFactura mapear(ResultSet rs) throws SQLException {
+        LineaFactura a = new LineaFactura();
+	 a.setIdLineaFactura(rs.getInt("id_linea_factura"));
+	 a.setIdFactura(rs.getInt("id_factura"));
+	 a.setIdTratamiento(rs.getInt("id_tratamiento"));
+	 a.setFecha(rs.getObject("fecha",LocalDate.class));
+	 a.setCantidad(rs.getInt("cantidad"));
+	 a.setPrecioTratamiento(rs.getDouble("precio_tratamiento"));
+	 a.setImporte(rs.getDouble("importe"));
+	  return a;
+    }
+
 
 }
