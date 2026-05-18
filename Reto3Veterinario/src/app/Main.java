68e492949387fe06ef28f09e03fce34a748c5748
1DAM <1DAM@A11-PC29>
2026-05-18 11:00:52 +0200

    Main 1 Empezando

--- Reto3Veterinario/src/app/Main.java
+++ Reto3Veterinario/src/app/Main.java
@@ -1,17 +1,11 @@
 package app;
 
-import dao.TratamientoDAO;
-import modelo.Tratamiento;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		TratamientoDAO tradao = new TratamientoDAO();
-		
-		for (Tratamiento tradlist: tradao.obtenerPorIdVeterinario(2)) {
-			System.out.println(tradlist);
-		}
+		System.out.println("Hola mundo");
 
 	}
 
