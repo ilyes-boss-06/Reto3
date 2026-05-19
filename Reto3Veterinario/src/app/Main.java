9c82277a01ef21ab29aabedb2e154b0daada99b9
ilyes-boss-06 <Boss@DESKTOP-7E1S7DH>
2026-05-19 12:48:25 +0200

    Main Principal completo

--- Reto3Veterinario/src/app/Main.java
+++ Reto3Veterinario/src/app/Main.java
@@ -1,10 +1,48 @@
 package app;
 
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("hola mundo");
+		Scanner sc = new Scanner(System.in);
+
+		// Alumno 1
+		Main1.ejercicio1();
+		Main1.ejercicio2(sc);
+
+		// Alumno 2
+		Main2.ejercicio3(sc);
+		Main2.ejercicio4(sc);
+
+		// Alumno 3
+		Main3.ejercicio5(sc);
+		Main3.ejercicio6();
+
+		// Alumno 1
+		Main1.ejercicio7(sc);
+		Main1.ejercicio8(sc);
+
+		// Alumno 2
+		Main2.ejercicio9(sc);
+		Main2.ejercicio10(sc);
+
+		// Alumno 3
+		Main3.ejercicio11(sc);
+		Main3.ejercicio12(sc);
+
+		// Alumno 1
+		Main1.ejercicio13(sc);
+		Main1.ejercicio14();
+
+		// Alumno 2
+		Main2.ejercicio15(sc);
+		Main2.ejercicio16(sc);
+
+		// Alumno 3
+		Main3.ejercicio17(sc);
+
+		sc.close();
 	}
 }
